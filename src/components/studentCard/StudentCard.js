17380e0592cf969000b455c14641c60ba56f7c43
HectorIlarraza <53978521+HectorIlarraza@users.noreply.github.com>
2022-07-01 12:19:45 -0500

    added toggle grades feature on studentCard

--- src/components/studentCard/StudentCard.js
+++ src/components/studentCard/StudentCard.js
@@ -1,22 +1,18 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { FaPlus } from "react-icons/fa";
+import { FaMinus } from "react-icons/fa";
 import "./StudentCard.scss";
 
 const StudentCard = ({student}) => {
     
+    // Props deconstructed
     const {pic, firstName, lastName, email, company, skill, grades} = student;
 
+    // Hooks
+    const [showGrades, setShowGrades] = useState(false)
+
     // Functions
-
     const calculateAverage = (grades) => {
-
-        // let sum = 0;
-
-        // grades.map(grade => {
-        //      sum += Number(grade);
-        //});
-
-        // return sum / grades/length;
 
         const sum = grades.reduce((a,b) => a + Number(b), 0);
         return sum / grades.length;
@@ -31,18 +27,30 @@ const StudentCard = ({student}) => {
                 <div className='studentCard__name'>
                     {`${firstName} ${lastName}`}
                 </div>
+                <div className='studentCard__infoLine'>
+                    Emails: {email}
+                </div>
+                <div className='studentCard__infoLine'>
+                    Company: {company}
+                </div>
+                <div className='studentCard__infoLine'>
+                    Skills: {skill}
+                </div>
+                <div className='studentCard__infoLine'>
+                    Average: {calculateAverage(grades)}%
+                </div>
+                <div className='studentCard__gradesList' style={{"display": showGrades ? true : false}}>
+                    {grades.map((grade, index) => {
+                        return (
+                            <div><span>Test {index + 1}:</span><span>{grade}%</span></div>
+                            )
+                        })}
+                </div>
             </div>
-            <div className='studentCard__infoLine'>
-                Emails: {email}
-            </div>
-            <div className='studentCard__infoLine'>
-                Company: {company}
-            </div>
-            <div className='studentCard__infoLine'>
-                Skills: {skill}
-            </div>
-            <div className='studentCard__infoLine'>
-                Average: {calculateAverage(grades)}%
+            <div className='studentCard__toggleIcons'>
+
+                {!showGrades && <FaPlus className='studentCard__toggleIcon' onClick={() => setShowGrades(true)} size="1.8em" />}
+                {showGrades && <FaMinus className='studentCard__toggleIcon' onClick={() => setShowGrades(false)} size="1.8em" />}
             </div>
         </div>
     )
