b0e93434193fb320c1b552bb0eff0f4ca308da08
HectorIlarraza <53978521+HectorIlarraza@users.noreply.github.com>
2022-07-26 23:36:43 -0500

    integrated student grades into StudentCard and display on FE, created a loader function to load grades data with low network speed

--- src/components/studentCard/StudentCard.js
+++ src/components/studentCard/StudentCard.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { FaPlus } from "react-icons/fa";
-import { FaMinus } from "react-icons/fa";
+import React, { useEffect, useState } from 'react';
+import { FaPlus, FaMinus } from "react-icons/fa";
+import { AiOutlineReload } from "react-icons/ai";
 import { Link } from  "react-router-dom";
 import SingleTextInput from '../singleTextInput/SingleTextInput.js';
 import "./StudentCard.scss";
@@ -8,27 +8,57 @@ import "./StudentCard.scss";
 const StudentCard = ({student}) => {
     
     // Props deconstructed
-    const {pic, firstname, lastname, email, company, skill, grades = []} = student;
+    const {pic, firstname, lastname, email, company, skill} = student;
 
     // Hooks
+    const [grades, setGrades] = useState([]);
     const [showGrades, setShowGrades] = useState(false);
+    const [gradesLoading, setGradesLoading] = useState(false);
     const [tags, setTags] = useState([]);
     const [tag, setTag] = useState("");
-
-    console.log(tag);
 
     // Functions
     const calculateAverage = (grades) => {
 
-        const sum = grades.reduce((a,b) => a + Number(b), 0);
-        return sum / grades.length;
+        const sum = grades.reduce((a,b) => a + Number(b.grade), 0);
+        return sum / grades.length; 
     };
 
-    const toggleGrades = (e) => {
+    const hideGrades = (e) => {
         e.preventDefault();
         e.stopPropagation();
-        setShowGrades(!showGrades);
+
+        setShowGrades(false);
     }
+
+    const fetchAndShowGrades = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        // do we already have the grades?
+        if(grades.length > 0){
+            setShowGrades(true);
+        }else{
+
+            setGradesLoading(true);
+    
+            const url = "http://localhost:9000/students/1/grades";
+    
+            fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                setGrades(data);
+                setGradesLoading(false);
+            })
+        }
+        
+    }
+    
+    useEffect(() => {
+        if(grades.length){
+            setShowGrades(!showGrades);
+        }
+    }, [grades])
 
     return (
         <div className='studentCard'>
@@ -49,21 +79,21 @@ const StudentCard = ({student}) => {
                 <div className='studentCard__infoLine'>
                     Skills: {skill}
                 </div>
-                <div className='studentCard__infoLine'>
-                    Average: {grades.length && calculateAverage(grades)}%
-                </div>
                 <div className='studentCard__gradesList' style={{"display": showGrades ? "block" : "none"}}>
+                    <div className='studentCard__gradeAverage'>
+                        Average: {grades.length && calculateAverage(grades)}%
+                    </div>
                     {grades.map((grade, index) => {
                         return (
-                                <div key={index}><span>Test {index + 1}:</span><span>{grade}%</span></div>
+                                <div key={index}><span>Test {index + 1}:</span><span>{grade.grade}%</span></div>
                             )
                         })}
                 </div>
             </div>
             <div className='studentCard__toggleIcons'>
-
-                {!showGrades && <FaPlus className='studentCard__toggleIcon' onClick={(e) => toggleGrades(e)} size="1.8em" />}
-                {showGrades && <FaMinus className='studentCard__toggleIcon' onClick={(e) => toggleGrades(e)} size="1.8em" />}
+                {gradesLoading && <AiOutlineReload className='studentCard__toggleIcon-spinning' size="1.8em" />}
+                {(!showGrades && !gradesLoading) && <FaPlus className='studentCard__toggleIcon' onClick={(e) => fetchAndShowGrades(e)} size="1.8em" />}
+                {(showGrades && !gradesLoading) && <FaMinus className='studentCard__toggleIcon' onClick={(e) => hideGrades(e)} size="1.8em" />}
             </div>
 
             </Link>
